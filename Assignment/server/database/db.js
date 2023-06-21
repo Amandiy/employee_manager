e1c055a369e0fea6d26a50f837d0346750a907c4
N.Amandi Yalapola <it21303470@my.sliit.lk>
2023-06-21 21:48:37 +0530

    commit

--- Assignment/server/database/db.js
+++ Assignment/server/database/db.js
@@ -3,8 +3,7 @@ import mongoose  from "mongoose";
 
 
 const Connection =async (username,password) => {
-    const URL =`mongodb+srv://amandi:amandiyalapola@crud-app.o72swm6.mongodb.net/supplier?retryWrites=true&w=majority`;
-    //`mongodb+srv://${username}:${password}@mernapp.cnpzawc.mongodb.net/MERNCRUD?retryWrites=true&w=majority`;
+    const URL =`mongodb+srv://amandi:amandiyalapola@crud-app.o72swm6.mongodb.net/employee?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true});
         console.log('Database Connected successfully');
